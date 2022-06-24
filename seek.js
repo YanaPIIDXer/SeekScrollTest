41fbff1b22bc8bd52a2e9098920243e9f4a4aefe
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-25 03:50:30 +0900

    シーク処理実装

--- seek.js
+++ seek.js
@@ -5,9 +5,15 @@ new Vue({
             items: [],
         }
     },
-    mounted: function () {
+    created: function () {
         for (let i = 1; i <= 100; i++) {
-            this.items.push(`ITEM ${i}`)
+            this.items.push(`ITEM${i}`)
         }
+    },
+    mounted: function () {
+        // シーク
+        const scrollable = document.getElementById('scrollable')
+        const target = document.getElementById('ITEM50')
+        scrollable.scrollTop = target.offsetTop
     }
 })
